2b6f35d2399c67c0e1b273b24c2c178829185222
Shubham Sunil Lonare <shubhamlonare25@gmail.com>
2026-01-29 18:13:39 +0530

    base of project....only navbar added

---
+++ src/app/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { Button, Typography } from '@mui/material'
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <Typography>hbjhbjh</Typography>
+        </>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import * as React from 'react';
+import Image from "next/image";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Logo from '../../public/logo.svg';
+
+import '../app/globals.css';
+
+function ResponsiveAppBar() {
+
+    return (
+        <AppBar position="static" sx={{ bgcolor: "white" }}>
+            <Container maxWidth="lg">
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+
+                    {/* Left: Logo */}
+                    <Image src={Logo} alt="Logo" width={120} height={40} />
+
+                    {/* Right: Tabs + Button */}
+                    <Box sx={{ display: "flex", gap: 2, }}>
+                        <Button>Features</Button>
+                        <Button>How It Works</Button>
+                        <Button>Benefits</Button>
+                        <Button>Login</Button>
+
+                        <Button variant="contained" color="primary" sx={{borderRadius: "10px"}}>
+                            Get Started
+                        </Button>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
